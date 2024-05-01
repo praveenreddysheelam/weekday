e12f771a171fbfe038c917fd3d23f3d4e6daef16
praveenreddysheelam <praveenreddysheelam199719@gmail.com>
2024-05-01 18:01:24 +0530

    Basic UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
+import JobCard from './JobCard';
 const Body = () => {
     const [listOfJobs,setListOfJobs]=useState(null);
     const myHeaders = new Headers();
@@ -12,6 +13,7 @@ const Body = () => {
         const data =await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
         const json= await data.json();
         setListOfJobs(json?.jdList);
+        console.log(listOfJobs);
     }
     useEffect(()=>{
         fetchData();
@@ -26,11 +28,21 @@ const Body = () => {
     { value: 'bangalore', label: 'Bangalore' },
     { value: 'mumbai', label: 'Mumbai' },
     { value: 'delhi ncr', label: 'Delhi' }]
+    if(!listOfJobs){
+       return;
+    }
   return (
     <div>
     <div className='flex m-4 p-4 '>
         <Select placeholder="Roles" options={job_options} isMulti />
         <Select placeholder="Location" className='mx-2 px-2 ' options={location_options} isMulti />
+    </div>
+    <div className='flex flex-wrap '>{ listOfJobs && 
+        listOfJobs.map((job)=> <div key={job.jdUid} className="max-w-md mx-2 mb-4">
+        <JobCard  jobData={job}></JobCard>
+        </div>
+    )
+    }
     </div>
     </div>
   )
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const JobCard = () => {
+const JobCard = (props) => {
+    const [expanded, setExpanded] = useState(false);
+    
+    const toggleExpanded = () => {
+    setExpanded(!expanded);
+    }
+    const jobData=props.jobData;
+    const {jobDetailsFromCompany,location,jobRole}=jobData;
+    const Capitalize= (str)=> {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
   return (
-    <div>JobCard</div>
-  )
+    <div className="bg-white rounded-lg shadow-md border border-gray-300 p-4 mb-4 w-80 h-196 ">
+    <div className='flex'>
+    <img src='https://etimg.etb2bimg.com/thumb/msid-103031053,imgsize-2088,width-1200,height=765,overlay-ethrsea/news/industry/weekday-secures-2-2m-funding-as-it-tackles-critical-trust-gap-in-hiring-tech-teams-with-its-reference-focus.jpg' alt={"company"} className="h-16 w-16 rounded-full flex-shrink-0 mr-4" />
+      <div>
+      <p className="text-gray-400 text-lg">Week Day</p>
+      <h2 className="text-lg">{Capitalize(jobRole)}</h2>
+      <p className="text-black-600 mb-2 font-bold">{Capitalize(location)}</p>
+      </div>
+    </div>
+    <div className="flex-grow"> 
+      <div  className=''>
+       <h3 className='font-semibold mt-4'> About Company </h3>
+       <h4 className='font-semibold'> About us</h4>
+      <p className=" ltext-sm text-gray-800 leading-snug ${expanded ? '' : 'opacity-100'}"  style={{ fontFamily: 'Arial, sans-serif' }}>{expanded ? jobDetailsFromCompany : `${jobDetailsFromCompany.substring(0, 300)}...`}</p>
+      </div>
+      {jobDetailsFromCompany.length > 300 && (
+        <button
+          className="text-blue-500 hover:text-blue-700"
+          onClick={toggleExpanded}
+        >
+          {expanded ? 'Show Less' : 'View Job'}
+        </button>
+      )}
+      <p className="text-gray-600">Experience Required: Not Given</p>
+      <div className='mt-4 flex justify-center'>
+      <a
+        href="/"
+        className="bg-teal-500 w-full font-semibold text-black py-2  text-center rounded-md inline-block mt-2 "
+      >
+        Easy Apply 
+      </a>
+      </div>
+      <div className='mt-4 flex justify-center'>
+     
+      <a
+        href="/"
+        className="bg-blue-700 w-full font-semibold text-white py-2  text-center rounded-md inline-block mt-2 "
+      >
+        
+        Unlock Referral asks
+      </a>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default JobCard
